76073c4af840d708e37e81a4f4f8397b31af972a
Stevhen <stevhen.work@gmail.com>
2020-09-19 19:38:35 +0700

    Initial Commit

---
+++ Covid19-Tracking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid19-Tracking
+//
+//  Created by Stevhen on 19/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Covid19-Tracking/Covid19_TrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Covid19_TrackingApp.swift
+//  Covid19-Tracking
+//
+//  Created by Stevhen on 19/09/20.
+//
+
+import SwiftUI
+
+@main
+struct Covid19_TrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
